60a74f9865d66189cb76e8cc18c1aeb00a69c04e
Solzar <orjan.karlsson@gmail.com>
2025-04-01 19:09:11 +0200

    Revert "Tagit bort minifier"
    This reverts commit 7e6f678f38ca9f52010976dbe56cf7da70d78142.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
+const htmlmin = require("html-minifier");
 
 const inputDir = path.resolve(__dirname, "./src");
 
@@ -62,6 +63,21 @@ module.exports = eleventyConfig => {
 		"includeWebpackAsset",
 		relativePathTag(webpackAssetContents)
 	);
+
+	if (process.env.ELEVENTY_ENV === "production") {
+		eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+			if (outputPath.endsWith(".html")) {
+				let minified = htmlmin.minify(content, {
+					useShortDoctype: true,
+					removeComments: true,
+					collapseWhitespace: true
+				});
+				return minified;
+			}
+
+			return content;
+		});
+	}
 
 	return {
 		dir: {
